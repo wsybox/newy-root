432b5e2cc5e01e26bcdbff2da82c1da45bd2cee8
wsybox <weisongyao310126@qq.com>
2024-06-06 21:15:09 +0800

    release

--- scripts/build.js
+++ scripts/build.js
@@ -1,66 +1,6 @@
 // scripts/build.js
-import { readdirSync, statSync } from 'fs'
 import { execa } from 'execa'
-import { createRequire } from 'node:module'
-import path from 'node:path'
-
-const require = createRequire(import.meta.url)
-const pkg = p => require(path.resolve(`packages/${p}/package.json`))
-
-/*
-const dirs = [
-  [ 'define', 'dsl', 'shared' ],
-  [ 'html', 'signal' ],
-  [ 'plugin-signal-extend' ],
-  [ 'newy' ]
-]
-*/
-const dirs = []
-const add = (dir, i) => {
-  if (!dirs[i]) dirs[i] = []
-  dirs[i].push(dir)
-}
-
-readdirSync('packages')
-  .filter(dir => statSync(`packages/${dir}`).isDirectory())
-  .map(dir => {
-    const dep = pkg(dir).dependencies
-
-    return {
-      dir,
-      deps: !dep
-        ? dep
-        : Object.keys(dep)
-            .filter(key => key.startsWith('@newy/') && dep[key] === 'workspace:^')
-            .map(k => k.slice(6))
-    }
-  })
-  .sort((a, b) => {
-    if (!a.deps) {
-      if (!b.deps) return 0
-      return -1
-    }
-    if (!b.deps) return 1
-    if (b.deps.includes(a.dir)) return -1
-    return 0
-  })
-  .forEach(({ dir, deps }) => {
-    if (!deps) return add(dir, 0)
-
-    let i,
-      j,
-      k = 0
-    for (i = 0; i < deps.length; i++) {
-      d: for (j = dirs.length - 1; j >= 0; j--) {
-        if (dirs[j].includes(deps[i])) {
-          if (k < j + 1) k = j + 1
-          break d
-        }
-      }
-    }
-
-    add(dir, k)
-  })
+import { run } from './get-dir'
 
 async function build(TARGET) {
   await execa('rollup', ['-c', '--environment', `TARGET:${TARGET}`], { stdio: 'inherit' })
@@ -75,8 +15,4 @@ function runParaller(_dirs) {
 }
 
 // 2、进行打包
-const _run = () => {
-  if (dirs.length > 0) runParaller(dirs.shift()).then(_run)
-}
-
-_run()
+run(runParaller)
